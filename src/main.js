15ec9619c3fab7e08516cb1ec2d5ffd7eb76f39a
sundada <956593305@qq.com>
2020-08-02 10:42:21 +0800

    test2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './myRouter'
-console.log('1111111111111')
+console.log('222222222')
 // import router from './router'
 
 Vue.config.productionTip = false
