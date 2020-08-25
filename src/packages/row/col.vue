9d5cccb12cf06c9954c67caed645e02298825ea0
sundada <956593305@qq.com>
2020-08-25 21:35:24 +0800

    一些练习

---
+++ src/packages/row/col.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>hahaahaha</div>
+</template>
+
+<script>
+export default {
+  // name: 'col',
+  // props: {
+  //   tag: {
+  //     type: String,
+  //     default: 'div'
+  //   },
+  //   span: String || Number,
+  //   offset: String || Number
+  // }
+}
+</script>
+
+<style lang="less" scoped>
+// .col {
+//   float: left;
+// }
+// .generator-col(24);
+// .generator-col(@n, @i : 1) when (@i < @n) {
+//   .col-@{i} {
+//     width: @i / @n * 100%;
+//   }
+//   .generator-col(@n, @i +1);
+// }
+</style>
